52f778040c2796ec2febf6c6e052bd5db51c85f9
Emerson Tenorio <emersontenorio1@gmail.com>
2025-07-28 20:17:21 -0300

    Instância de estrutura do projeto.

---
+++ src/br/com/imdbcatalogo/domain/Filme.java
@@ -0,0 +1,70 @@
+package domain;
+
+import java.util.List;
+
+public class Filme {
+
+    private Long id;
+    private String nome;
+    private String dataLancamento;
+    private Double orcamento;
+    private String descricao;
+    private Diretor diretor;
+    private List<Ator> atores;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Double getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(Double orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    public List<Ator> getAtores() {
+        return atores;
+    }
+
+    public void setAtores(List<Ator> atores) {
+        this.atores = atores;
+    }
+}
