8aec28f9c269f5f0177a4e9d381e0de06024b12a
Fabricia Ribeiro <fabriciaribeiro@gmail.com>
2025-08-11 21:45:47 -0300

    classe filme

--- src/main/java/br/com/imdbcatalogo/domain/Filme.java
+++ src/main/java/br/com/imdbcatalogo/domain/Filme.java
@@ -1,5 +1,6 @@
 package com.imdbcatalogo.domain;
 
+import com.imdbcatalogo.utils.DateUtils;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -58,12 +59,22 @@ public class Filme {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Filme: ").append(nome).append("\n");
-        sb.append("Data de Lançamento: ").append(dataLancamento).append("\n");
-        sb.append("Orçamento: $").append(orcamento).append("\n");
+        sb.append("Data de Lançamento: ").append(new DateUtils().formatDate(dataLancamento)).append("\n");
+        sb.append("Orçamento: $").append(String.format("%.2f", orcamento)).append("\n");
         sb.append("Descrição: ").append(descricao).append("\n");
         sb.append("Diretor: ").append(diretor.getNome()).append("\n");
         sb.append("Atores: ");
-        atores.forEach(ator -> sb.append(ator.getNome()).append(", "));
+        if (!atores.isEmpty()) {
+            for (int i = 0; i < atores.size(); i++) {
+                sb.append(atores.get(i).getNome());
+                if (i < atores.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+        } else {
+            sb.append("Nenhum ator associado.");
+        }
         return sb.toString();
     }
 }
+
